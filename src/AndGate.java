8393e613a6333a7e7f57620f4a14d8797088e453
acareau <careau.alexandre@gmail.com>
2023-06-24 16:02:14 -0400

    Comments and non-binary input handling

--- src/AndGate.java
+++ src/AndGate.java
@@ -1,8 +1,15 @@
+// Neural node class definition for AND gate object
 public class AndGate {
+    // Two input variables
     int x1, x2;
+    // Weights and bias preset for AND gate behaviour
     int w1 = 1, w2 = 1, b = -1;
 
     public int generateOutput(int x1, int x2) {
+        // Correct for non-binary inputs
+        if (x1 > 1) x1 = 1;
+        if (x2 > 1) x2 = 1;
+        // Output is 1 only if both inputs are 1
         return (x1*w1 + x2*w2 + b) > 0 ? 1 : 0;
     }
 }
--- src/AndGateDriver.java
+++ src/AndGateDriver.java
@@ -1,8 +1,11 @@
+// Test driver for AND gate neural node
 public class AndGateDriver {
 
     public static void main(String[] args){
         int x1, x2;
+        // Create neural node object from AND gate class
         AndGate testGate = new AndGate();
+        // Generate 10 sets of inputs, call object function to generate output and print results
         for (int i = 0; i < 10; i++) {
             x1 = (int)Math.round(Math.random());
             x2 = (int)Math.round(Math.random());
