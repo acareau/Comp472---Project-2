31c420173b0a70362ea70f4e736f15fb64c9b967
UndercoverPoutine <briac.cordelle@gmail.com>
2023-06-25 13:04:07 -0400

    reorganize

---
+++ Python/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, mean_squared_error
+from tqdm import tqdm_notebook
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.datasets import make_blobs
+
+
