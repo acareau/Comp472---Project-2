afd32f52c9ced235452faa799e8d333815543cf1
UndercoverPoutine <briac.cordelle@gmail.com>
2023-06-25 18:32:55 -0400

    python part

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,136 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.colors
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, mean_squared_error
-from tqdm import tqdm_notebook
 
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.datasets import make_blobs
+class NeuralNetwork:
+    def __init__(self, inputs, hidden_size, outputs):
+        self.inputs = inputs
+        self.hidden_size = hidden_size
+        self.outputs = outputs
+
+        # randomly initialize weights and biases for the hidden and output layers
+        self.hidden_weights = np.random.randn(self.inputs, self.hidden_size)
+        self.hidden_bias = np.zeros((1, self.hidden_size))
+        self.weights_output = np.random.randn(self.hidden_size, self.outputs)
+        self.bias_output = np.zeros((1, self.outputs))
+
+    def forward(self, X):
+        # Forward propagation through the network
+
+        # Hidden layer activation
+        self.hidden_layer_activation = np.dot(X, self.hidden_weights) + self.hidden_bias
+        self.hidden_layer_output = self.relu(self.hidden_layer_activation)
+
+        # Output layer activation
+        self.output_layer_activation = np.dot(self.hidden_layer_output, self.weights_output) + self.bias_output
+        self.output_layer_output = self.sigmoid(self.output_layer_activation)
+
+        return self.output_layer_output
+
+    def relu(self, x):
+        # ReLU activation function
+        return np.maximum(0, x)
+
+    def sigmoid(self, x):
+        # Sigmoid activation function
+        return 1 / (1 + np.exp(-x))
+
+    def mean_squared_error(self, y_pred, y_true):
+        return np.mean((y_pred - y_true) ** 2)
+
+    def train(self, X, y, epochs, learning_rate):
+        for epoch in range(epochs):
+            # forward pass
+            output = self.forward(X)
+
+            # get the loss
+            loss = self.mean_squared_error(output, y)
+
+            # backpropagation below!
+
+            # calculate difference between output and actual answer
+            d_output = output - y
+
+            # Calculate the derivative of the loss with respect to the hidden layer
+            d_hidden = np.dot(d_output, self.weights_output.T) * (self.hidden_layer_output > 0)
+
+            # Update weights and biases using gradient descent
+            self.weights_output -= learning_rate * np.dot(self.hidden_layer_output.T, d_output)
+            self.bias_output -= learning_rate * np.sum(d_output, axis=0, keepdims=True)
+            self.hidden_weights -= learning_rate * np.dot(X.T, d_hidden)
+            self.hidden_bias -= learning_rate * np.sum(d_hidden, axis=0, keepdims=True)
+
+            # track progress through loss
+            if epoch % 10 == 0:
+                print(f"Epoch {epoch}: Loss = {loss:.4f}")
 
 
+# training X
+X = np.array([[1, 1, 1, 0, 0],
+    [1,1,1,0,1],
+    [0,0,0,1,0],
+    [0,0,0,1,1],
+    [0,0,1,0,0],
+    [0,0,1,0,1],
+    [0,0,1,1,0],
+    [0,0,1,1,1],
+    [0,1,0,0,0],
+    [0,1,0,0,1],
+    [0,1,0,1,0],
+    [0,1,0,1,1],
+    [0,1,1,0,0],
+    [0,1,1,0,1],
+    [0,1,1,1,0],
+    [0,1,1,1,1],
+    [1,0,0,0,0],
+    [1,0,0,0,1],
+    [1,0,0,1,0],
+    [1,0,0,1,1],
+    [1,0,1,0,0],
+    [1,0,1,0,1],
+    [1,0,1,1,0],
+    [1,0,1,1,1]])
+# training answers
+y = np.array([[1],
+    [1],
+    [0],
+    [0],
+    [1],
+    [1],
+    [1],
+    [1],
+    [0],
+    [0],
+    [0],
+    [0],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1],
+    [1]])
+
+
+# create the network with input size 5, hidden layers 2 and output size 1
+nn = NeuralNetwork(inputs=5, hidden_size=5, outputs=1)
+
+# train the network using our X
+nn.train(X, y, epochs=100, learning_rate=0.1)
+
+# test the new inputs
+new_input = np.array([[1,1,0,0,0],
+    [1,1,0,0,1],
+    [0,0,0,0,0],
+    [0,0,0,0,1],
+    [1,1,0,1,0],
+    [1,1,0,1,1],
+    [1,1,1,1,0],
+    [1,1,1,1,1]])
+prediction = nn.forward(new_input)
+
+print("Predictions: ")
+print(prediction)
--- README.md
+++ README.md
@@ -1,23 +1,39 @@
 # Comp472---Project-2
 
-Question 1 (JAVA):
+Question 1 (Java):
 
 a) How many hidden layers have you used?
 
   None. This is a simple function, two input nodes to one output node.
 
-b) Weight of each input node is set to 1, as they are both equally important. Bias is set to -1 to insure that the function returns 0 if only one of the inputs is equal to 1.
+b) What are the weights and biases of each node?
+
+Weight of each input node is set to 1, as they are both equally important. Bias is set to -1 to ensure that the function returns 0 if only one of the inputs is equal to 1.
 
 
 Question 2 (Python):
 
 a) How many hidden layers have you used? And why?
 
+Just one, because the training set is pretty small and not very complex. 
+
 b) How many nodes in each hidden layer and why that number of nodes in particular?
+
+5 nodes because there are 5 inputs. 
 
 c) What is the activation function that you used and why? Did you use the same activation function in all layers? Why?
 
+Both. The ReLu is used in the hidden layer and the Sigmoid is used in the output layer. 
+We use the ReLu in the hidden layer because it is more efficient, and it reduces overfitting. 
+We use the Sigmoid in the output layer because it is better at classifying binary outputs because of the way
+it "squashes" values between 0 and 1 (giving a probability). It also gives a clear decision boundary at 0.5.
+
 d) What learning algorithm did you use to train the neural net and why?
+
+Batch gradient descent. In this gradient descent, the whole data set is used in each epoch, this can be expensive 
+but in our case it's ok because the data set is small. This method tends to be more accurate than Schotastic 
+gradient descent or mini-batch gradient descent as it considers the whole dataset each time. It is not 
+used as much due to its high computing cost but in this case, with a small dataset, this method made sense.
 
 e) Can you use one hidden layer only to solve this problem? If yes, how many nodes are you going to have in it? And why?
 
@@ -28,3 +44,5 @@ f) Can we use 5 hidden layers? Is that a good idea? Justify your answer.
   Yes, it's possible to use 5 layers. No, it wouldn't be a good idea. Since the datasets can be solved with a single function, adding layers and nodes needlessly adds time and space complexity to the network.
 
 g) How did the neural net do in classifying the testing set? Comment on how good or bad it learned the function from the training set.
+
+It did good. 
